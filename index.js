b85e91d829727787e7fcca63e7dc7ff7067a51fe
Tejas Singh <notnormalcoder@gmail.com>
2023-09-15 21:34:25 +0530

    added env-removed string

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+require('dotenv').config(); 
+
 const { ApolloServer } = require('apollo-server');
-const { PubSub } = require('graphql-subscriptions'); 
+const { PubSub } = require('graphql-subscriptions');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
 
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI; 
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req, pubsub })
+  context: ({ req }) => ({ req, pubsub }),
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB Connected');
     return server.listen({ port: PORT });
@@ -26,5 +28,5 @@ mongoose
     console.log(`Server running at ${res.url}`);
   })
   .catch(err => {
-    console.error(err)
-  })
+    console.error(err);
+  });
